480e2696108241f80ccc079935a8005c5e265cac
Bruno Pires <bruno.angeloti@terra.com.br>
2021-10-29 19:09:50 -0300

    tentando arrumar bug

--- components/CardDepositions.js
+++ components/CardDepositions.js
@@ -6,7 +6,7 @@ export function CardDepositions(){
             
             <h1>“Extraordinária”</h1>
             <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
             </p>
             
             <div className={styles.info}>
--- containers/About.js
+++ containers/About.js
@@ -8,7 +8,7 @@ export function About(props){
                 <h1>Pâmela Cristina Pires</h1>
                 <p>Graduada em Educação Física - MULTIVIX</p>
                 <p>Meu nome é Pâmela Cristina, tenho 25 anos e moro em Jacaraípe - Serra. Sou recém formada em educação física e faixa marrom em kickboxing.
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not.</p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not.</p>
                 <Bar type="1"/>
             </div>
             <img src="/Pamela2.png"/>          
