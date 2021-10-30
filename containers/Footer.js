208cf3eab0e175d5bdd2805fe203ff0543ff596f
Bruno Pires <bruno.angeloti@terra.com.br>
2021-10-29 21:09:24 -0300

    faltando algumas funcionalidades

--- containers/Footer.js
+++ containers/Footer.js
@@ -3,7 +3,7 @@ import styles from '../styles/containers/Footer.module.scss'
 export function Footer(props){
     return(
         <div ref={props.referencia} className={styles.containerFooter}>          
-            <p>Feito com ❤️ por <a href="https://github.com/BrunoAngeloti"><span>Bruno Angeloti</span></a></p>
+            <p>Feito com ❤️ por <a href="https://github.com/BrunoAngeloti"><span>Bruno Angeloti</span></a> e <a href="https://github.com/maluarmini"><span>Maria Luiza</span></a></p>
         </div>
     )
 }
